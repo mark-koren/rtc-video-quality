5a7066e3d8b181d0ba27a1fa0a08048900859645
mark-koren <mkorenfhspltw09@gmail.com>
2019-04-05 22:35:21 -0700

    added all default derf SD presets

--- input/video_presets.py
+++ input/video_presets.py
@@ -4,3 +4,125 @@ ALL = [
 		'https://media.xiph.org/video/derf/y4m/football_qcif_15fps.y4m',
 		'https://media.xiph.org/video/derf/y4m/news_qcif.y4m'
 		]
+
+DERF_SD_QCIF_ALL = [
+		"https://media.xiph.org/video/derf/y4m/akiyo_qcif.y4m",
+		"https://media.xiph.org/video/derf/y4m/bowing_qcif.y4m",
+		"https://media.xiph.org/video/derf/y4m/bridge_close_qcif.y4m",
+		"https://media.xiph.org/video/derf/y4m/bridge_far_qcif.y4m",
+		"https://media.xiph.org/video/derf/y4m/bus_qcif_15fps.y4m",
+		"https://media.xiph.org/video/derf/y4m/carphone_qcif.y4m",
+		"https://media.xiph.org/video/derf/y4m/city_qcif_15fps.y4m",
+		"https://media.xiph.org/video/derf/y4m/claire_qcif-5.994Hz.y4m",
+		"https://media.xiph.org/video/derf/y4m/coastguard_qcif.y4m",
+		"https://media.xiph.org/video/derf/y4m/container_qcif.y4m",
+		"https://media.xiph.org/video/derf/y4m/crew_qcif_15fps.y4m",
+		"https://media.xiph.org/video/derf/y4m/deadline_qcif.y4m",
+		"https://media.xiph.org/video/derf/y4m/flower_garden_422_qcif.y4m",
+		"https://media.xiph.org/video/derf/y4m/football_422_qcif.y4m",
+		"https://media.xiph.org/video/derf/y4m/football_qcif_15fps.y4m",
+		"https://media.xiph.org/video/derf/y4m/foreman_qcif.y4m",
+		"https://media.xiph.org/video/derf/y4m/galleon_422_qcif.y4m",
+		"https://media.xiph.org/video/derf/y4m/grandma_qcif.y4m",
+		"https://media.xiph.org/video/derf/y4m/hall_objects_qcif.y4m",
+		"https://media.xiph.org/video/derf/y4m/hall_monitor_qcif.y4m",
+		"https://media.xiph.org/video/derf/y4m/harbour_qcif_15fps.y4m",
+		"https://media.xiph.org/video/derf/y4m/husky_qcif.y4m",
+		"https://media.xiph.org/video/derf/y4m/intros_422_qcif.y4m",
+		"https://media.xiph.org/video/derf/y4m/mad900_qcif.y4m",
+		"https://media.xiph.org/video/derf/y4m/miss_am_qcif.y4m",
+		"https://media.xiph.org/video/derf/y4m/mobile_calendar_422_qcif.y4m",
+		"https://media.xiph.org/video/derf/y4m/mother_daughter_qcif.y4m",
+		"https://media.xiph.org/video/derf/y4m/news_qcif.y4m",
+		"https://media.xiph.org/video/derf/y4m/pamphlet_qcif.y4m",
+		"https://media.xiph.org/video/derf/y4m/paris_qcif.y4m",
+		"https://media.xiph.org/video/derf/y4m/salesman_qcif.y4m",
+		"https://media.xiph.org/video/derf/y4m/sign_irene_qcif.y4m",
+		"https://media.xiph.org/video/derf/y4m/silent_qcif.y4m",
+		"https://media.xiph.org/video/derf/y4m/soccer_qcif_15fps.y4m",
+		"https://media.xiph.org/video/derf/y4m/students_qcif.y4m",
+		"https://media.xiph.org/video/derf/y4m/suzie_qcif.y4m",
+		"https://media.xiph.org/video/derf/y4m/trevor_qcif.y4m",
+		"https://media.xiph.org/video/derf/y4m/vtc1nw_422_qcif.y4m"
+]
+
+DERF_SD_CIF_ALL = [
+		"https://media.xiph.org/video/derf/y4m/akiyo_cif.y4m",
+		"https://media.xiph.org/video/derf/y4m/bowing_cif.y4m",
+		"https://media.xiph.org/video/derf/y4m/bridge_close_cif.y4m",
+		"https://media.xiph.org/video/derf/y4m/bridge_far_cif.y4m",
+		"https://media.xiph.org/video/derf/y4m/bus_cif.y4m",
+		"https://media.xiph.org/video/derf/y4m/city_cif.y4m",
+		"https://media.xiph.org/video/derf/y4m/coastguard_cif.y4m",
+		"https://media.xiph.org/video/derf/y4m/crew_cif.y4m",
+		"https://media.xiph.org/video/derf/y4m/deadline_cif.y4m",
+		"https://media.xiph.org/video/derf/y4m/flower_cif.y4m",
+		"https://media.xiph.org/video/derf/y4m/flower_garden_422_cif.y4m",
+		"https://media.xiph.org/video/derf/y4m/football_422_cif.y4m",
+		"https://media.xiph.org/video/derf/y4m/football_cif.y4m",
+		"https://media.xiph.org/video/derf/y4m/galleon_422_cif.y4m",
+		"https://media.xiph.org/video/derf/y4m/hall_monitor_cif.y4m",
+		"https://media.xiph.org/video/derf/y4m/harbour_cif.y4m",
+		"https://media.xiph.org/video/derf/y4m/highway_cif.y4m",
+		"https://media.xiph.org/video/derf/y4m/husky_cif.y4m",
+		"https://media.xiph.org/video/derf/y4m/ice_cif.y4m",
+		"https://media.xiph.org/video/derf/y4m/intros_422_cif.y4m",
+		"https://media.xiph.org/video/derf/y4m/mad900_cif.y4m",
+		"https://media.xiph.org/video/derf/y4m/mobile_cif.y4m",
+		"https://media.xiph.org/video/derf/y4m/mobile_calendar_422_cif.y4m",
+		"https://media.xiph.org/video/derf/y4m/mother_daughter_cif.y4m",
+		"https://media.xiph.org/video/derf/y4m/news_cif.y4m",
+		"https://media.xiph.org/video/derf/y4m/pamphlet_cif.y4m",
+		"https://media.xiph.org/video/derf/y4m/paris_cif.y4m",
+		"https://media.xiph.org/video/derf/y4m/sign_irene_cif.y4m",
+		"https://media.xiph.org/video/derf/y4m/silent_cif.y4m",
+		"https://media.xiph.org/video/derf/y4m/soccer_cif.y4m",
+		"https://media.xiph.org/video/derf/y4m/students_cif.y4m",
+		"https://media.xiph.org/video/derf/y4m/tempete_cif.y4m",
+		"https://media.xiph.org/video/derf/y4m/vtc1nw_422_cif.y4m",
+		"https://media.xiph.org/video/derf/y4m/washdc_422_cif.y4m",
+		"https://media.xiph.org/video/derf/y4m/waterfall_cif.y4m"
+]
+
+DERF_SD_4CIF_ALL = [
+		"https://media.xiph.org/video/derf/y4m/city_4cif.y4m",
+		"https://media.xiph.org/video/derf/y4m/crew_4cif.y4m",
+		"https://media.xiph.org/video/derf/y4m/harbour_4cif.y4m",
+		"https://media.xiph.org/video/derf/y4m/ice_4cif.y4m",
+		"https://media.xiph.org/video/derf/y4m/soccer_4cif.y4m"
+]
+
+DERF_SD_SIF_ALL = [
+		"https://media.xiph.org/video/derf/y4m/garden_sif.y4m",
+		"https://media.xiph.org/video/derf/y4m/mobile_sif.y4m",
+		"https://media.xiph.org/video/derf/y4m/stefan_sif.y4m",
+		"https://media.xiph.org/video/derf/y4m/tennis_sif.y4m"
+]
+
+DERF_SD_4SIF_ALL = [
+		"https://media.xiph.org/video/derf/y4m/flower_garden_422_4sif.y4m",
+		"https://media.xiph.org/video/derf/y4m/football_422_4sif.y4m",
+		"https://media.xiph.org/video/derf/y4m/galleon_422_4sif.y4m",
+		"https://media.xiph.org/video/derf/y4m/mobile_calendar_422_4sif.y4m",
+		"https://media.xiph.org/video/derf/y4m/vtc1nw_422_4sif.y4m",
+		"https://media.xiph.org/video/derf/y4m/washdc_422_4sif.y4m"
+]
+
+DERF_SD_NTSC_ALL = [
+		"https://media.xiph.org/video/derf/y4m/flower_garden_422_ntsc.y4m",
+		"https://media.xiph.org/video/derf/y4m/football_422_ntsc.y4m",
+		"https://media.xiph.org/video/derf/y4m/galleon_422_ntsc.y4m",
+		"https://media.xiph.org/video/derf/y4m/mobile_calendar_422_ntsc.y4m",
+		"https://media.xiph.org/video/derf/y4m/vtc1nw_422_ntsc.y4m",
+		"https://media.xiph.org/video/derf/y4m/washdc_422_ntsc.y4m"
+]
+
+DERF_SD_QCIF_MONO_ALL = [
+		"https://media.xiph.org/video/derf/y4m/coastguard_qcif_mono.y4m",
+		"https://media.xiph.org/video/derf/y4m/foreman_qcif_mono.y4m"
+]
+
+DERF_SD_SIF_MONO_ALL = [
+		"https://media.xiph.org/video/derf/y4m/mobile_sif_mono.y4m",
+		"https://media.xiph.org/video/derf/y4m/stefan_sif_mono.y4m"
+]
